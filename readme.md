77b74691c722134595f8a7fc197b9cd33a274213
Jake <chimpytk@gmail.com>
2013-02-04 04:22:15 +0000

    added jquery, tidied up, added readme

---
+++ readme.md
@@ -0,0 +1,85 @@
+# Tock #
+
+A javscript timer/countdown clock. 
+
+Based on code by James Edwards:
+http://sitepoint.com/creating-accurate-timers-in-javascript/
+
+## Features ##
+
+* Can be used to count up from 0:00 or down from a given time. 
+* Can call a callback function every tick (10 milliseconds) and (for countdown clocks) when the clock reaches 0:00.
+* Pretty much accurate to about 10 milliseconds (0.01 seconds). 
+
+## How does it work? ##
+
+#### Make some html to show the clock. ####
+
+      <button id="start">Start</button> 
+      <button id="stop">Stop</button> 
+      <input id="clock" value="10:00">
+
+#### Instantiate a Tock ####
+
+Now in our javascript we make a new instance of Tock and assign it to a variable called *timer*. We'll pass in some options as an object literal while we're at it. *All options are...optional.*
+
+    var timer = new Tock({
+        countdown: true,
+        interval: 10,
+        callback: someCallbackFunction,
+        complete: someCompleteFunction
+    });
+
+* If *countdown* is true clock will count down from a given time, otherwise it will count up from 0:00. Default: false.
+
+* *interval* is how often, in milliseconds, that the clock will tick. Default: 10.
+
+* *callback* is called every time the clock ticks, once per *interval*. Default: null;
+
+* *complete* is called once the clock reaches 0:00. (only used if countdown = true). Default: null.
+
+#### Control your clock ####
+
+Now you'll need some way of controlling your clock. Let's set up some buttons *(using jQuery)*.
+
+    // Add a start button.
+    $('#start').on('click', function() {
+	    timer.start($('#clock').val());
+	});
+
+Note that we get the time from the clock input and pass it to the start function as the start time.
+
+    // Add a stop button.
+    $('#stop').on('click', function() {
+	    timer.stop();
+	});
+
+
+#### Add some callback functions ####
+
+Let's replace "someCallbackFunction" above with an anonymous function. Here we use the *lap()* method to get the current clock time in milliseconds, pass that through *msToTime()* to format it nicely and then insert it into the text input we made earlier.
+
+    callback: function () {
+        $('#clock').val(timer.msToTime(timer.lap()));
+    }
+
+As we are have set *countdown* to *true* we can also replace "someCompleteFunction" with an anonymous function.
+
+    complete: function () {
+        alert("Time's up!");
+    } 
+
+## Options ##
+
+ * countdown, *bool*
+ * interval, *int*
+ * callback, *function*
+ * complete, *function*
+
+## Methods ##
+
+ * reset()
+ * start(time)
+ * stop()
+ * lap()
+ * msToTime(ms)
--- tock.js
+++ tock.js
@@ -1,174 +1,195 @@
 /**
-* Tock by Mr Chimp - github.com/mrchimp
+* Tock by Mr Chimp - github.com/mrchimp/tock
 * Based on code by James Edwards:
 *    sitepoint.com/creating-accurate-timers-in-javascript/
 */
 
 function Tock(options) {
-    //you probably dont want to change this
+    this.go = false;
     this.interval = options.interval || 10;
-    this.type = options.type || 'timer';
-    if (options.callback != undefined) {
+    this.countdown = options.countdown || false;
+    this.final_time = 0;
+    if (options.callback !== undefined) {
         this.callback = options.callback;
-    }
-    else {
+    } else {
         this.callback = function () {};
     }
-    this.go = false;
-    this.countdown_start = 0;
+    if (options.complete !== undefined) {
+        this.complete = options.complete;
+    } else {
+        this.complete = function () {};
+    }
 }
 
-Tock.prototype.tick = function () {
-  console.log(this.name);
-
-  // indecrement the clock
-  this.time += this.interval;
-  // get _accurate_ number of ticks since start
-  // i.e. number of ticks their should have been
-  this.elapsed = Math.floor(this.time / this.interval)/ 10;
-  
-  // convert to float if necessary
-  if(Math.round(this.elapsed) == this.elapsed) { this.elapsed += '.0'; }
-  
-  // find the difference between 
-  var diff = (Date.now() - this.start_time) - this.time;
-  
-  // get the current scope to pass to the timeout
-  var t = this;
-  
-  // run the callback function if there is one
-  if (this.callback != undefined) {
-    this.callback(this);
-  }
-  
-  // if we're still counting, keep ticking
-  if (this.go) {
-    this.timeout = setTimeout(function () {t.tick();}, (this.interval - diff));
-  }
-};
-
-Tock.prototype.tickDown = function () {
-  console.log(this.name);
-  
-  // indecrement the clock
-  this.time += this.interval;
-  
-  this.clock_time = this.start_time
-  
-  // get _accurate_ number of ticks since start
-  // i.e. number of ticks their should have been
-  this.elapsed = Math.floor(this.time / this.interval)/ 10;
-  
-  // convert to float if necessary
-  if(Math.round(this.elapsed) == this.elapsed) { this.elapsed += '.0'; }
-  
-  // find the difference between 
-  //var diff = (Date.now() - this.start_time) - this.time;
-  var diff = (Date.now() - this.start_time) - this.time;
-  
-  // run the callback function if there is one
-  if (this.callback != undefined) {
-    this.callback(this);
-  }
-  
-  if (this.duration_ms - this.time < 0) {
-    alert('end!');
-    this.go = false;
-  } 
-  
-  var t = this; // get the current scope to pass to the timeout
-  
-  // if we're still counting, keep ticking
-  if (this.go) {
-    this.timeout = setTimeout(function () {t.tickDown();}, (this.interval - diff));
-  }
-};
-
-// Reset the clock
+/**
+ * Reset the clock
+ */
 Tock.prototype.reset = function () {
-  this.stop();
-  this.start_time = 0;
-  this.time = 0;
-  this.elapsed = '0.0';
+    this.stop();
+    this.start_time = 0;
+    this.time = 0;
+    this.elapsed = '0.0';
 };
 
-// Called internally - use start() instead
-Tock.prototype._startCountdown = function (start) {  
-  
-  var time_split = start.split(':');
-  this.duration_ms = parseInt(time_split[0]) * 60000;
-  
-  if (time_split.length) {
-    this.duration_ms += parseInt(time_split[1]) * 1000;
-  }
-
-  this.start_time = Date.now();
-  this.end_time = this.start_time + this.duration_ms;
-  this.time = 0;
-  this.elapsed = '0.0';
-  this.go = true;
-  
-  var t = this;
-  
-  this.timeout = setTimeout(function () { t.tickDown(); }, 100);
+/**
+ * Start the clock.
+ */
+Tock.prototype.start = function (time) {
+    if (this.countdown === true) {
+        this._startCountdown(time);
+    } else {
+        this._startTimer();
+    }
 };
 
-// Called internally - use start() instead
-Tock.prototype._startTimer = function () {
-  this.start_time = Date.now();
-  this.time = 0;
-  this.elapsed = '0.0';
-  this.go = true;
-  
-  var t = this;
-  
-  this.timeout = setTimeout(function () { t.tick(); }, 100);
+/**
+ * Stop the clock.
+ */
+Tock.prototype.stop = function () {
+    this.go = false;
+    this.final_time = (Date.now() - this.start_time);
+    window.clearTimeout(this.timeout);
 };
 
-// Start the clock
-Tock.prototype.start = function(start) {
-  if (this.type == 'countdown') {
-    this._startCountdown(start);
-  } else {
-    this._startTimer();
-  }
-};
+/** 
+ * Get the current clock time in ms.
+ * Use with Tock.msToTime() to make it look nice.
+ */
+Tock.prototype.lap = function () {
+    if (this.go) {
+        var now;
 
-// Stop the clock
-Tock.prototype.stop = function() {
-  this.go = false;
-  this.final_time = (Date.now() - this.start_time);
-  clearTimeout(this.timeout);
-};
+        if (this.countdown === true) {
+            now = this.duration_ms - (Date.now() - this.start_time);
+        } else {
+            now = (Date.now() - this.start_time);
+        }
 
-// Get the current time
-Tock.prototype.lap = function() {
-  if (this.type == 'timer') {
-    var now = (Date.now() - this.start_time);
-  } else {
-    var now = this.duration_ms - (Date.now() - this.start_time);
-  }
-  
-  if (this.go) {
-    return now;
-  }
-  
-  if (this.final_time != undefined) {
+        return now;
+    }
+
     return this.final_time;
-  } else {
-    return '';
-  }
 };
 
-// Format milliseconds as a string
-Tock.prototype.msToTime = function(ms) {
-  var milliseconds = ms % 1000,
-      seconds = Math.floor((ms /1000) % 60).toString(), minutes = Math.floor((ms / (60 * 1000)) % 60).toString();
-    if (seconds.length == 1) {
+/**
+ * Format milliseconds as a string.
+ */
+Tock.prototype.msToTime = function (ms) {
+    var milliseconds = ms % 1000,
+        seconds = Math.floor((ms / 1000) % 60).toString(),
+        minutes = Math.floor((ms / (60 * 1000)) % 60).toString();
+    if (seconds.length === 1) {
         seconds = '0' + seconds;
     }
-    if (minutes.length == 1) {
+    if (minutes.length === 1) {
         minutes = '0' + minutes;
     }
     return minutes + ":" + seconds + ":" + milliseconds;
+};
+
+/**
+ * Convert a time string "mm:ss" to milliseconds
+ */
+Tock.prototype.timeToMS = function (time) {
+    var time_split = time.split(':')
+    
+    ms = parseInt(time_split[0], 10) * 60000;
+
+    if (time_split.length) {
+        ms += parseInt(time_split[1], 10) * 1000;
+    }
+    
+    return ms;
+}
+
+/**
+ * Called every tick for timer (count up) clocks.
+ * i.e. once every this.interval ms
+ */
+Tock.prototype._tick = function () {
+
+    // Increment the clock
+    this.time += this.interval;
+
+    // Get _accurate_ number of ticks since start
+    // i.e. number of ticks their should have been
+    this.elapsed = Math.floor(this.time / this.interval) / 10;
+
+    // Convert to float if necessary
+    if (Math.round(this.elapsed) === this.elapsed) { this.elapsed += '.0'; }
+
+    // Check against system clock
+    var t = this,
+        // Get the current scope to pass to the timeout
+        diff = (Date.now() - this.start_time) - this.time;
+
+        // Run the callback function if there is one
+    if (this.callback !== undefined) {
+        this.callback(this);
+    }
+
+    // If we're still counting, keep ticking
+    if (this.go) {
+        this.timeout = window.setTimeout(function () { t._tick(); }, (this.interval - diff));
+    }
+};
+
+/**
+ * Called every tick for countdown clocks.
+ * i.e. once every this.interval ms
+ */
+Tock.prototype._tickDown = function () {
+    this.time += this.interval;
+    this.clock_time = this.start_time;
+    this.elapsed = Math.floor(this.time / this.interval) / 10;
+
+    if (Math.round(this.elapsed) === this.elapsed) { this.elapsed += '.0'; }
+
+    var t = this,
+        diff = (Date.now() - this.start_time) - this.time;
+
+    if (this.callback !== undefined) {
+        this.callback(this);
+    }
+
+    if (this.duration_ms - this.time < 0) {
+        this.final_time = 0;
+        this.go = false;
+        this.complete();
+    }
+
+    if (this.go) {
+        this.timeout = window.setTimeout(function () { t._tickDown(); }, (this.interval - diff));
+    }
+};
+
+/**
+ * Called by Tock internally - use start() instead
+ */
+Tock.prototype._startCountdown = function (duration) {
+
+    var t = this;
+
+    this.duration_ms = duration;
+    this.start_time = Date.now();
+    this.end_time = this.start_time + this.duration;
+    this.time = 0;
+    this.elapsed = '0.0';
+    this.go = true;
+
+    this.timeout = window.setTimeout(function () { t._tickDown(); }, 100);
+};
+
+/**
+ * Called by Tock internally - use start() instead
+ */
+Tock.prototype._startTimer = function () {
+    this.start_time = Date.now();
+    this.time = 0;
+    this.elapsed = '0.0';
+    this.go = true;
+
+    var t = this;
+
+    this.timeout = window.setTimeout(function () { t._tick(); }, 100);
 };
