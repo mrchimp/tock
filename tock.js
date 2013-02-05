d47babd26d97930a58aeb38fe025845e3873f129
Jake <chimpytk@gmail.com>
2013-02-05 02:00:10 +0000

    further refinements
    Improved time string to ms convertion, consolidated tick functions,
    simplified some code.

--- tock.js
+++ tock.js
@@ -9,22 +9,17 @@ function Tock(options) {
     this.interval = options.interval || 10;
     this.countdown = options.countdown || false;
     this.final_time = 0;
-    if (options.callback !== undefined) {
-        this.callback = options.callback;
-    } else {
-        this.callback = function () {};
-    }
-    if (options.complete !== undefined) {
-        this.complete = options.complete;
-    } else {
-        this.complete = function () {};
-    }
+    this.callback = options.callback;
+    this.complete = options.complete;
 }
 
 /**
  * Reset the clock
  */
 Tock.prototype.reset = function () {
+    if (this.countdown) {
+        return false;
+    }
     this.stop();
     this.start_time = 0;
     this.time = 0;
@@ -35,7 +30,7 @@ Tock.prototype.reset = function () {
  * Start the clock.
  */
 Tock.prototype.start = function (time) {
-    if (this.countdown === true) {
+    if (this.countdown) {
         this._startCountdown(time);
     } else {
         this._startTimer();
@@ -59,7 +54,7 @@ Tock.prototype.lap = function () {
     if (this.go) {
         var now;
 
-        if (this.countdown === true) {
+        if (this.countdown) {
             now = this.duration_ms - (Date.now() - this.start_time);
         } else {
             now = (Date.now() - this.start_time);
@@ -88,59 +83,42 @@ Tock.prototype.msToTime = function (ms) {
 };
 
 /**
- * Convert a time string "mm:ss" to milliseconds
+ * Convert a time string to milliseconds
+ * Todo: handle this a bit better
+ *
+ * Possible inputs:
+ * MM:SS
+ * MM:SS:ms
+ * yyyy-mm-dd HH:MM:SS.ms
  */
 Tock.prototype.timeToMS = function (time) {
-    var time_split = time.split(':')
-    
-    ms = parseInt(time_split[0], 10) * 60000;
 
-    if (time_split.length) {
-        ms += parseInt(time_split[1], 10) * 1000;
-    }
+    var ms = new Date(time).getTime();
     
+    if (!ms) {
+        var time_split = time.split(':'),
+            ms;
+        
+        ms = parseInt(time_split[0], 10) * 60000;
+
+        if (time_split.length > 1) {
+            ms += parseInt(time_split[1], 10) * 1000;
+        }
+
+        if (time_split.length > 2) {
+            ms += parseInt(time_split[2], 10);
+        }
+    }
+
     return ms;
-}
-
-/**
- * Called every tick for timer (count up) clocks.
- * i.e. once every this.interval ms
- */
-Tock.prototype._tick = function () {
-
-    // Increment the clock
-    this.time += this.interval;
-
-    // Get _accurate_ number of ticks since start
-    // i.e. number of ticks their should have been
-    this.elapsed = Math.floor(this.time / this.interval) / 10;
-
-    // Convert to float if necessary
-    if (Math.round(this.elapsed) === this.elapsed) { this.elapsed += '.0'; }
-
-    // Check against system clock
-    var t = this,
-        // Get the current scope to pass to the timeout
-        diff = (Date.now() - this.start_time) - this.time;
-
-        // Run the callback function if there is one
-    if (this.callback !== undefined) {
-        this.callback(this);
-    }
-
-    // If we're still counting, keep ticking
-    if (this.go) {
-        this.timeout = window.setTimeout(function () { t._tick(); }, (this.interval - diff));
-    }
 };
 
 /**
  * Called every tick for countdown clocks.
  * i.e. once every this.interval ms
  */
-Tock.prototype._tickDown = function () {
+Tock.prototype._tick = function () {
     this.time += this.interval;
-    this.clock_time = this.start_time;
     this.elapsed = Math.floor(this.time / this.interval) / 10;
 
     if (Math.round(this.elapsed) === this.elapsed) { this.elapsed += '.0'; }
@@ -152,14 +130,14 @@ Tock.prototype._tickDown = function () {
         this.callback(this);
     }
 
-    if (this.duration_ms - this.time < 0) {
+    if (this.countdown && (this.duration_ms - this.time < 0)) {
         this.final_time = 0;
         this.go = false;
         this.complete();
     }
 
     if (this.go) {
-        this.timeout = window.setTimeout(function () { t._tickDown(); }, (this.interval - diff));
+        this.timeout = window.setTimeout(function () { t._tick(); }, (this.interval - diff));
     }
 };
 
@@ -167,17 +145,14 @@ Tock.prototype._tickDown = function () {
  * Called by Tock internally - use start() instead
  */
 Tock.prototype._startCountdown = function (duration) {
-
-    var t = this;
-
     this.duration_ms = duration;
     this.start_time = Date.now();
     this.end_time = this.start_time + this.duration;
     this.time = 0;
     this.elapsed = '0.0';
     this.go = true;
-
-    this.timeout = window.setTimeout(function () { t._tickDown(); }, 100);
+    var t = this;
+    this.timeout = window.setTimeout(function () { t._tick(); }, 100);
 };
 
 /**
@@ -188,8 +163,6 @@ Tock.prototype._startTimer = function () {
     this.time = 0;
     this.elapsed = '0.0';
     this.go = true;
-
     var t = this;
-
     this.timeout = window.setTimeout(function () { t._tick(); }, 100);
 };
